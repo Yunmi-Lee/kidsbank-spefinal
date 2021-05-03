51e38f14e17f1ad4a4f22c0e71e01805701d2c73
yunmi.lee <yunmi.lee@iiitb.org>
2021-05-03 15:43:19 +0530

    modify db config

--- config.php
+++ config.php
@@ -1,16 +1,19 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'mysqldb-service');
+define('DB_SERVER', 'mysql-cluster-ip-service');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_NAME', 'testdb');
+define('DB_PORT', '3306');
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
  
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
+} else {
+    echo "Connected successfully";
 }
 ?>
