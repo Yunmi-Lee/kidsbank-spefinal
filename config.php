66ab43c26d285f932e46da4e096baa3391d8138d
yunmi.lee <yunmi.lee@iiitb.org>
2021-05-03 16:58:39 +0530

    modify db config

--- config.php
+++ config.php
@@ -13,7 +13,7 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
-} else {
-    echo "Connected successfully";
 }
+
+
 ?>
